930efd2b3349f82bb87eaaa3dcdc17d01ebef61c
Rachit Patel <rachitp509@gmail.com>
2023-11-03 11:29:36 +0530

    bug fixs

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -1,11 +1,11 @@
-const express = require("express");
-const { auth } = require("../middlewares/auth");
-const { createAuctionOrder } = require("../controllers/orderControllers");
+// const express = require("express");
+// const { auth } = require("../middlewares/auth");
+// const { createAuctionOrder } = require("../controllers/orderControllers");
 
-const router = express.Router();
+// const router = express.Router();
 
-router.post("/create-auction-order", auth, createAuctionOrder);
+// router.post("/create-auction-order", auth, createAuctionOrder);
 // router.post("/capture-payment/:orderId", auth, captureOrderPayment);
 // router.post("/webhook", bookingWebhook);
 
-module.exports = router;
+// module.exports = router;
