11a7ccdfb2756ae70a0bad44564a370bee039f6f
Rachit Patel <rachitp509@gmail.com>
2023-11-10 11:37:58 +0530

    bug fixes

--- controllers/auctionControllers.js
+++ controllers/auctionControllers.js
@@ -204,7 +204,7 @@ exports.getAuctionDetails = async (req, res) => {
   try {
     const auction = await Auction.findById(req.params.auctionId)
       .populate("car")
-      .populate("seller", "name email phoneNumber profilePicUrl")
+      .populate("seller", "sellerCity carLocationCity")
       .populate("highest_bid", "bid_amount");
 
     if (!auction) return res.status(404).json({ message: "Auction not found" });
